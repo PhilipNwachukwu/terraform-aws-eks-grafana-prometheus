4db35daef3b25abb134440d29decde0ff52d9dae
PhilipNwachukwu <82297069+PhilipNwachukwu@users.noreply.github.com>
2023-03-27 11:09:58 +0000

    Update namespace.tf

--- namespace.tf
+++ namespace.tf
@@ -1,5 +1,5 @@
 resource "kubernetes_namespace" "grafana" {
-  depends_on = [var.mod_dependency]
+  depends_on = [module.eks]
   count      = (var.enabled && var.create_namespace_grafana && var.namespace_grafana != "kube-system") ? 1 : 0
 
   metadata {
@@ -8,7 +8,7 @@ resource "kubernetes_namespace" "grafana" {
 }
 
 resource "kubernetes_namespace" "prometheus" {
-  depends_on = [var.mod_dependency]
+  depends_on = [module.eks]
   count      = (var.enabled && var.create_namespace_prometheus && var.namespace_prometheus != "kube-system") ? 1 : 0
 
   metadata {
