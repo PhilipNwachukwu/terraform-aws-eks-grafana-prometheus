2d33b07024370662e0b27c8724a76a4e86773218
PhilipNwachukwu <82297069+PhilipNwachukwu@users.noreply.github.com>
2023-03-29 02:39:08 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -22,6 +22,37 @@ provider "aws" {
 
 provider "kubernetes" {}
 
+data "aws_region" "current" {}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "aws" {
+  region = data.aws_region.current.id
+  alias  = "default" # this should match the named profile you used if at all
+}
+
+provider "kubernetes" {
+  experiments {
+    manifest_resource = true
+  }
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+  
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
+}
 
 #   required_providers {
 #     aws        = ">= 3.13"
