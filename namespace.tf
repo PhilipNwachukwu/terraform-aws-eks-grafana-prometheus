4e89baaa9c0ef70c387598ef196a6cd8a03c7816
PhilipNwachukwu <82297069+PhilipNwachukwu@users.noreply.github.com>
2023-03-29 15:06:56 +0000

    Update namespace.tf

--- namespace.tf
+++ namespace.tf
@@ -1,19 +1,3 @@
-resource "null_resource" "kubeconfig" {
-  triggers = {
-    always = timestamp()
-  }
-
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = <<EOT
-      set -e
-      echo 'Applying Auth ConfigMap with kubectl...'
-      aws eks wait cluster-active --name '${local.cluster_name}'
-      aws eks update-kubeconfig --name ${EKS_CLUSTER_NAME} --region ${REGION}
-    EOT
-  }
-}
-
 resource "kubernetes_namespace" "grafana" {
 #   depends_on = [module.eks]
   count      = (var.enabled && var.create_namespace_grafana && var.namespace_grafana != "kube-system") ? 1 : 0
